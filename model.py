4c7e8a13100315e1fb6e0bf3467f3d637ccc1701
crabarca <caabarca1@uc.cl>
2018-07-08 13:56:28 -0400

    keras example

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ x_test = np.random.random((100, 20))
 y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
 
 model = Sequential()
-model.add(Conv2D(32, 7, activation='relu', input_shape=(110, 90, 1)))
+model.add(Conv2D(32, 7, activation='relu', input_shape=(110, 90, 3)))
 model.add(MaxPool2D(strides=2))
 model.add(Conv2D(64, 7, activation='relu'))
 model.add(MaxPool2D(strides=2))
@@ -26,9 +26,9 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 
-# model.fit(x_train, y_train,
-#           epochs=20,
-#           batch_size=128)
-# score = model.evaluate(x_test, y_test, batch_size=128)
+model.fit(x_train, y_train,
+          epochs=20,
+          batch_size=128)
+score = model.evaluate(x_test, y_test, batch_size=128)
 
 
